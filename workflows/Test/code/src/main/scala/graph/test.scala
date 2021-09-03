a57cf1a8c7b7b7c681b216d84246a685ed582dd1
yixiaow <464437275@qq.com>
2021-09-03 03:20:41 +0000

    New Commit for Workflow Code

---
+++ workflows/Test/code/src/main/scala/graph/test.scala
@@ -0,0 +1,46 @@
+package graph
+
+import io.prophecy.libs._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import config.ConfigStore._
+import udfs.UDFs._
+import udfs._
+import graph._
+
+@Visual(id = "test", label = "test", x = 71, y = 76, phase = 0)
+object test {
+
+  @UsesDataset(id = "4265", version = 0)
+  def apply(spark: SparkSession): Source = {
+    import spark.implicits._
+
+    val out = Config.fabricName match {
+      case "dev" =>
+        val schemaArg = StructType(
+          Array(
+            StructField("customer_id",       IntegerType, false),
+            StructField("first_name",        StringType,  false),
+            StructField("last_name",         StringType,  false),
+            StructField("phone",             StringType,  false),
+            StructField("email",             StringType,  false),
+            StructField("country_code",      StringType,  false),
+            StructField("account_open_date", StringType,  false),
+            StructField("account_flags",     StringType,  false)
+          )
+        )
+        spark.read
+          .format("json")
+          .schema(schemaArg)
+          .load("dbfs:/Prophecy/464437275@qq.com/CustomersDatasetInput.csv")
+          .cache()
+      case _ => throw new Exception(s"The fabric is not handled")
+    }
+
+    out
+
+  }
+
+}
